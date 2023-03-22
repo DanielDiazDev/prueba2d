f02619e74ecaea4f33f98288fcb2349b38aca650
DanielDiazDev <danieldiazdev00@gmail.com>
2023-03-22 12:49:55 -0300

    Ioksd

---
+++ Assets/Pruega.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pruega : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Añadasd");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
